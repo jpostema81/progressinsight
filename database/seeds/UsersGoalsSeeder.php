7bdcd0a28671aacc513bcd1fe55c76ed77f4abc7
Jeroen Postema <jeroen@script.nl>
2020-03-10 20:31:49 +0100

    minor update

--- database/seeds/UsersGoalsSeeder.php
+++ database/seeds/UsersGoalsSeeder.php
@@ -18,7 +18,6 @@ class UsersGoalsSeeder extends Seeder
         
         $users = User::all();
         $users = User::where('email', 'mathijslohr@ziggo.nl')->get();
-        dd($users);
         $learningGoals = LearningGoal::all();
         $progress_level_id = ProgressLevel::orderBy('percentage', 'asc')->first()->id;
 
