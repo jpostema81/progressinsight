e18f7990944e0300f020dbba4614949f87afc711
Jeroen Postema <jeroen@script.nl>
2020-03-10 13:52:11 +0100

    add db backup

--- database/seeds/UsersGoalsSeeder.php
+++ database/seeds/UsersGoalsSeeder.php
@@ -16,8 +16,7 @@ class UsersGoalsSeeder extends Seeder
     {
         DB::table('users_learning_goals')->delete();
         
-        // $users = User::all();
-        $users = User::where('email', 'mathijslohr@ziggo.nl');
+        $users = User::all();
         $learningGoals = LearningGoal::all();
         $progress_level_id = ProgressLevel::orderBy('percentage', 'asc')->first()->id;
 
