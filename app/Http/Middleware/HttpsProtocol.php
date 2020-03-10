4bd7d9e814f99ee5c1b36f78030f0c1fb277d460
Jeroen Postema <jeroen@script.nl>
2020-03-10 19:55:43 +0100

    add logging to https middleware

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -11,21 +11,18 @@ use Illuminate\Http\Request;
 //
 // * The current request comes with no secure protocol (http)
 // * If your environment is equals to production. So, just adjust the settings according to your preferences.
-
+//
+// more info at: https://stackoverflow.com/questions/28402726/laravel-5-redirect-to-https
 class HttpsProtocol {
 
     public function handle($request, Closure $next)
     {
         $request->setTrustedProxies([$request->getClientIp()], Request::HEADER_X_FORWARDED_ALL); 
-        $request->secure() ? $secure = "true" : $secure = "false";
-        Log::debug("\$request->secure(): $secure");
 
         if (!$request->secure() && App::environment() === 'production') {
-            Log::debug("redirecting from http to https");
             return redirect()->secure($request->getRequestUri());
         }
 
-        Log::debug("no http to https redirect");
         return $next($request); 
     }
 }
