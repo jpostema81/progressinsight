97b6663fd9dd4282046cc36bdfdb0d0800a82ffa
Jeroen Postema <jeroen@script.nl>
2020-06-25 16:08:30 +0200

    add migration for user table activation columns

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,8 +20,6 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->boolean('activated')->default(false);
-            $table->string('activation_token')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
